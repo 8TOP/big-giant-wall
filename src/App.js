b61d7d8a286f4f504c5e10117a9a021cebadc391
Joshua Milus <redrooster9@gmail.com>
2019-11-23 13:46:01 -0800

    url correction again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const initialState = {
 };
 
 //const backendUrl = (process.env.BEURL ? process.env.BEURL: "http://localhost:3000");
-const backendUrl = "https://big-giant-wall-backend.herokuapp.com/";
+const backendUrl = "https://big-giant-wall-backend.herokuapp.com";
 
 class App extends React.Component {
   constructor(props) {
